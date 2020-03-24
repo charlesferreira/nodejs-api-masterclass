e4d42bd2124e56c2fff8e38bc767c66bfbf8cab6
Charles Ferreira <charles.dev@live.com>
2020-03-24 08:54:12 -0300

    Calculate the average cost

--- seeder.js
+++ seeder.js
@@ -18,14 +18,18 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // Read JSON files
-const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
-const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    await Course.create(courses);
+    // await Course.create(courses);
 
     console.log('Data imported...'.green.inverse);
   } catch (err) {
